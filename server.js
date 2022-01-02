c877b1d45b97144a0240f340761895e1382831a0
Luke Barrier <lukebb0215@gmail.com>
2022-01-01 20:35:25 -0500

    Init

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.static(`${__dirname}/dist`));
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost${PORT != 80 ? ":"+PORT : ""}/`);
+});
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import "./app.scss"
+
+const App = () => {
+    return (
+        <div id="app">
+            <center><h1>Hello, React!</h1></center>
+        </div>
+    )
+}
+
+export default App
