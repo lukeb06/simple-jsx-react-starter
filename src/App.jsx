4623ea51b464ce800328bf177acc7e437b8cd7d9
Luke Barrier <lukebb0215@gmail.com>
2022-01-21 18:10:43 -0500

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import "./app.scss"
 
 const App = () => {
     return (
-        <div id="app">
-            <center><h1>Hello, React!</h1></center>
-        </div>
+        <>
+            <center>Hello, React!</center>
+        </>
     )
 }
 
