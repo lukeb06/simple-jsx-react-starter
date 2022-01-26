3103f7093559bf2efc669b95bffce14ddb7de928
Luke Barrier <lukebb0215@gmail.com>
2022-01-25 19:14:47 -0500

    Fix

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import "./app.scss"
+
+const App = () => {
+    return (
+        <>
+            <center>Hello, React!</center>
+        </>
+    )
+}
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import $ from "simple-jsx-react"
+
+import App from "./App.jsx"
+
+ReactDOM.render(
+    <>
+        <App />
+    </>,
+    $("#root")
+)
