b64b256899b4eb05302d4adb13b4cbcff791be1a
Luke <44011185+FroggyYT@users.noreply.github.com>
2022-05-20 10:54:20 -0400

    Revert "stuff"
    This reverts commit 997c7b2d9b378d98f140d83bb6c67a533afd4bff.

--- server.js
+++ server.js
@@ -3,22 +3,8 @@ const app = express();
 
 const PORT = 8080;
 
-const cookieParser = require("cookie-parser");
-
 app.use(express.static(`${__dirname}/dist`));
-app.use(cookieParser());
-
-const jsondb = require("simple-json-db");
-
-const db = new jsondb("users.json");
-
-const validateUser = (email, password) => (db.has(email) && db.get(email).password == password);
-
-app.get("/validateUser", (req, res) => {
-    const { email, password } = req.cookies;
-    res.send(validateUser(email, password));
-});
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost${PORT != 80 ? ":"+PORT : ""}/`);
-}); 
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import "./app.scss"
 
-import Login from "./Login.jsx"
-import Signup from "./Signup.jsx"
-import Home from "./Home.jsx"
-
 const App = () => {
-    // React state for showing the login page
-    const [showLogin, setShowLogin] = useState(true);
-
-    // React state for showing the home page
-    const [showHome, setShowHome] = useState(false);
-
-    const showLoginPage = () => {
-        setShowLogin(true);
-    }
-
-    const showSignupPage = () => {
-        setShowLogin(false);
-    }
-
-    const doShowHomePage = async () => {
-        const rawData = fetch("/validateUser");
-        const valid = await rawData.json();
-        if (valid) {
-            setShowHome(true);
-        }
-    }
-
     return (
         <>
-            {/* login/signup div */}
-            {!showHome ? (<div className="login-signup">
-                <form className="login-signup-form" action="">
-                    <div className="login-signup-form-container">
-                        <input onClick={showLoginPage} className="login-signup-form-input" type="radio" name="loginSignupRadio" id="loginRadio" value="Login" defaultChecked />
-                        <label className="login-signup-form-label" htmlFor="loginRadio">Login</label>
-                    </div>
-                    <div className="login-signup-form-container">
-                        <input onClick={showSignupPage} className="login-signup-form-input" type="radio" name="loginSignupRadio" id="signupRadio" value="Signup" />
-                        <label className="login-signup-form-label" htmlFor="signupRadio">Signup</label>
-                    </div>
-                </form>
-                {/* login div */}
-                {showLogin && <Login doShowHomePage={doShowHomePage} />}
-                {/* signup div */}
-                {!showLogin && <Signup doShowHomePage={doShowHomePage} />}
-            </div>) : <Home />}
+            <center>Hello, React!</center>
         </>
     )
 }
