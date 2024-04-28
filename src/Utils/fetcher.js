de6196d23744446f36419806bad2a3b0f1102c19
Luke Barrier <lukebb0215@gmail.com>
2024-04-28 17:59:15 -0400

    Vite

--- src/Utils/fetcher.js
+++ src/Utils/fetcher.js
@@ -1,12 +1,17 @@
-const API_PORT = 3001;
+const API_PORT = 3021;
 const API_URL = `${window.location.protocol}//${window.location.hostname}:${API_PORT}/`;
 const endpoint = (path) => `${API_URL}${path}`;
 
 const fetcher = async (path, options={}, textMode=false) => {
     return new Promise(async (resolve, reject) => {
-        const response = await fetch(endpoint(path), options);
-        const data = textMode ? await response.text() : await response.json();
-        resolve(data);
+        try {
+            const response = await fetch(endpoint(path), options);
+            const data = textMode ? await response.text() : await response.json();
+            resolve(data);
+        } catch (e) {
+            reject(e);
+        }
+        
     });
 }
 
